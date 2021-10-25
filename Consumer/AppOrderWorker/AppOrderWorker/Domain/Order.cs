8cedaab50cf904ad1f837f1a203ce9bbb8414b58
Yuri Fernandes <yuri.fernandes@primecontrol.com.br>
2021-10-25 11:12:58 -0300

    Adicionado Consumer das mensagens para o RabbitMQ

---
+++ Consumer/AppOrderWorker/AppOrderWorker/Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOrderWorker.Domain
+{
+    public sealed class Order
+    {
+        public int OrderNumber { get; set; }
+        public string ItemName { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Consumer/AppOrderWorker/AppOrderWorker/Program.cs
@@ -0,0 +1,51 @@
+﻿using AppOrderWorker.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace AppOrderWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "OrderQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        var order = System.Text.Json.JsonSerializer.Deserialize<Order>(message);
+
+                        Console.WriteLine($"Order: {order.OrderNumber}|{order.ItemName}|{order.Price:N2}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        //Logger
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+                channel.BasicConsume(queue: "OrderQueue",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
