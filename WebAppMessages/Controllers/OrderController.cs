bb3836eb755501fd221c1c47c116da0a7457f23c
Yuri Fernandes <yuri.fernandes@primecontrol.com.br>
2021-10-25 11:00:38 -0300

    Criação do publicador de mensagens para o RabbitMQ

---
+++ WebAppMessages/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+using WebAppMessages.Domain;
+
+namespace WebAppMessages.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult InsertOrder(Order order)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "OrderQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = JsonSerializer.Serialize(order);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "OrderQueue",
+                                         basicProperties: null,
+                                         body: body);
+                }
+                return Accepted(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro ao tentar criar um novo pedido", ex);
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
